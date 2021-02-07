4bcb4f708ff95193fb5754876610c23c9bbfc8a2
rakib <19-39358-1@student.aiub.edu>
2021-02-07 12:38:44 +0600

    print all even numbers

---
+++ task1/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            for (int i = 0; i <= n; i += 2)
+            {
+                Console.WriteLine("the even numbers are:" + i);
+            }
+        }
+    }
+}
