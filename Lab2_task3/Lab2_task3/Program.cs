3cb8503a3a2ae977483e6ad6336afaa4520fbc35
rakib <19-39358-1@student.aiub.edu>
2021-02-07 13:27:18 +0600

    labtask1

---
+++ Lab2_task3/Lab2_task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab2_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 0, sum2 = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum1 = sum1 + i;
+                }
+                else
+                {
+                    sum2 = sum2 + i;
+                }
+            }
+            Console.WriteLine("Sum of odd number (1-100): " + sum2);
+            Console.WriteLine("Sum of even number(1-100) : " + sum1);
+            Console.WriteLine();
+           
+        }
+    }
+}
